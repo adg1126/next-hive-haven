a71700ef892a6ca013160e78e96f29d8a3d00902
adg1126 <gutierrez.almiradyan@gmail.com>
2024-01-15 14:00:42 +0800

    boilerplate for pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Footer from "@/components/Footer";
+import { AnimatePresence } from "framer-motion";
+import UserNavbar from "@/components/Navbar/UserNavbar";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import { monsterrat } from "@/styles/fonts";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Provider store={store}>
+			<main
+				className={`${monsterrat.variable} font-mont dark:bg-dark bg-light w-full min-h-screen`}
+			>
+				<UserNavbar />
+				<AnimatePresence mode='wait'>
+					<Component {...pageProps} />
+				</AnimatePresence>
+				<Footer />
+			</main>
+		</Provider>
+	);
 }
