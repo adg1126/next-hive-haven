958f6315be7aaa84cf50ccfc4400ae4005fa9909
adg1126 <gutierrez.almiradyan@gmail.com>
2024-01-16 10:12:23 +0800

    Code clean up

---
+++ src/content/content.js
@@ -0,0 +1,40 @@
+import productPic3 from "../../public/images/product-pic-3.jpg";
+import productPic1 from "../../public/images/product-pic-1.jpg";
+import productPic2 from "../../public/images/product-pic-2.jpg";
+import productPic4 from "../../public/images/product-pic-4.jpg";
+
+export const productArr = [
+	{ title: "Standard Beeswax Wrapper", img: productPic1 },
+	{ title: "Green Beeswax Wrapper", img: productPic2 },
+	{ title: "Honeycomb Pattern Beeswax Wrapper", img: productPic3 },
+	{ title: "Pattern Beeswax Wrapper", img: productPic4 },
+];
+
+export const taglineArr = [
+	{ title: "Wrap it right" },
+	{ title: "Seal it tight" },
+	{ title: "Bee-friendly delight" },
+];
+
+export const productDescriptionArr = [
+	{
+		title: "Need",
+		paragraph: `A natural and biodegradable alternative to traditional plastic packaging that is reusable, compostable, and made from a renewable resource.`,
+		img: productPic1,
+	},
+	{
+		title: "Situation",
+		paragraph: `Reusable and biodegradable wraps, made from organic cotton, coconut oil, tree resin, and natural beeswax, provide a solution to the problem of single-use plastic waste`,
+		img: productPic2,
+	},
+	{
+		title: "Person",
+		paragraph: `Consumers, food producers and retailers, environmentalists`,
+		img: productPic3,
+	},
+	{
+		title: "USP",
+		paragraph: `Beeswax-based items offer a more environmentally conscious option for packaging, surpassing the eco-friendliness of traditional plastic wrappers.`,
+		img: productPic3,
+	},
+];
