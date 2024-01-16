1d132a026dc0c8e43015e2fb4f127968f3d9c28d
adg1126 <gutierrez.almiradyan@gmail.com>
2024-01-16 10:11:06 +0800

    Home page layout done

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Head from "next/head";
 import productPic3 from "../../public/images/product-pic-3.jpg";
+import productPic1 from "../../public/images/product-pic-1.jpg";
 import {
 	AnimatedTextSpring,
 	AnimatedTextTypeWriter,
 } from "@/components/AnimatedText";
-import Link from "next/link";
 import PageTransition from "@/components/PageTransition";
 import { raleway } from "@/styles/fonts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBullseye } from "@fortawesome/free-solid-svg-icons";
+import {
+	productArr,
+	taglineArr,
+	productDescriptionArr,
+} from "../content/content";
+import ProductCard from "@/components/Card/ProductCard";
+import CardWIthOverlay from "@/components/Card/CardWIthOverlay";
 
 export default function Home() {
 	const [mode, setMode] = useState("");
@@ -28,32 +37,112 @@ export default function Home() {
 			</Head>
 
 			<PageTransition />
-			<main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-				<Image
-					alt='beeswax wrapper'
-					src={productPic3}
-					layout='fill'
-					objectFit='cover'
-					className={`w-full h-screen flex items-center`}
-				/>
-				<div
-					className={`w-full h-screen bg-gray-200 flex items-center`}
-					style={{ backgroundImage: `url(${productPic3})` }}
-				>
-					<div className='w-1/2 h-screen bg-light dark:bg-dark opacity-90 flex flex-col items-center justify-center'>
-						<div className='w-5/6 flex flex-col space-y-10'>
-							<AnimatedTextTypeWriter
-								text={`Newly Launched`}
-								className={`${raleway.className} font-semibold italic text-3xl xl:text-5xl lg:text-6xl md:text-5xl sm:text-3xl text-left lg:text-center`}
-							/>
-							<AnimatedTextTypeWriter
-								text={`Hive Haven Beeswax Wraps`}
-								className={`${raleway.className} font-black italic text-6xl xl:text-5xl lg:text-6xl md:text-5xl sm:text-3xl text-left lg:text-center`}
-							/>
+			<main className='flex flex-col items-center text-dark w-full min-h-screen dark:text-light'>
+				{/* Section 1 */}
+				<section className='w-full h-screen flex items-center'>
+					<Image
+						alt='beeswax wrapper'
+						src={productPic3}
+						layout='fill'
+						objectFit='cover'
+						className={`w-full h-screen flex items-center`}
+					/>
+					<div
+						className={`w-full h-screen bg-gray-200 flex items-center`}
+						style={{ backgroundImage: `url(${productPic3})` }}
+					>
+						<div className='w-1/2 h-screen bg-light dark:bg-dark opacity-90 flex flex-col items-center justify-center'>
+							<div className='w-5/6 flex flex-col space-y-10'>
+								<AnimatedTextTypeWriter
+									text={`Newly Launched`}
+									className={`${raleway.className} font-semibold italic text-3xl xl:text-5xl lg:text-6xl md:text-5xl sm:text-3xl text-left lg:text-center`}
+								/>
+								<AnimatedTextTypeWriter
+									text={`Hive Haven Beeswax Wraps`}
+									className={`${raleway.className} font-black italic text-6xl xl:text-5xl lg:text-6xl md:text-5xl sm:text-3xl text-left lg:text-center`}
+								/>
+								<div>
+									{taglineArr.map((e, i) => (
+										<div
+											key={i}
+											className='flex flex-row items-center space-x-4'
+										>
+											<FontAwesomeIcon icon={faBullseye} />
+											<div className='font-medium'>{e.title}</div>
+										</div>
+									))}
+								</div>
+							</div>
+						</div>
+						<div className={`w-1/2 h-screen bg-primary opacity-70`}></div>
+					</div>
+				</section>
+
+				{/* Section 2 */}
+				<section className='w-full h-screen flex justify-center py-24'>
+					<div className='w-4/5 flex flex-col space-y-20 items-center top-10'>
+						<AnimatedTextSpring
+							text='New Arrivals'
+							className={`${raleway.className} ${raleway.className} font-black italic text-5xl xl:text-5xl lg:text-6xl md:text-4xl sm:text-3xl text-center`}
+						/>
+						<div className='flex w-full overflow-x-scroll space-x-4'>
+							{productArr.map((p, i) => (
+								<ProductCard
+									key={i}
+									{...p}
+								/>
+							))}
 						</div>
 					</div>
-					<div className={`w-1/2 h-screen bg-primary opacity-70`}></div>
-				</div>
+				</section>
+
+				{/* Section 3 */}
+				<section className='w-full h-screen bg-gray-200 flex justify-center items-center relative z-0'>
+					<Image
+						alt='beeswax wrapper'
+						src={productPic1}
+						objectFit='cover'
+						className={`w-full h-screen flex items-center`}
+					/>
+					<div className='bg-gradient-to-r from-light dark:from-dark absolute inset-0 h-screen flex flex-col items-center justify-center w-3/5'>
+						<div className='flex flex-col space-y-10 px-16'>
+							<AnimatedTextTypeWriter
+								text={`Product Description`}
+								className={`${raleway.className} font-semibold italic text-3xl xl:text-5xl lg:text-6xl md:text-5xl sm:text-3xl text-left lg:text-center`}
+							/>
+							<p className='font-medium w-full md:text-sm text-left lg:text-center'>
+								{`Beeswax wonders – the eco-chic solution to embrace a greener
+								tomorrow. Crafted from nature's finest beeswax, our products not
+								only protect your cherished items but also safeguard our planet.
+								These enchanting beeswax wraps and containers wrap your goods in
+								a warm embrace, keeping them fresh and guilt-free, while bidding
+								farewell to single-use plastics. Elevate your sustainable
+								lifestyle with a touch of buzzworthy elegance and show the world
+								that eco-conscious living can be both exquisite and
+								conscientious.`}
+							</p>
+						</div>
+					</div>
+				</section>
+
+				{/* Section 4 */}
+				<section className='w-full h-full flex justify-center py-24'>
+					<div className='w-4/5 flex flex-col space-y-20 items-center top-10'>
+						<AnimatedTextTypeWriter
+							text={`Why Choose Hive Haven?`}
+							className={`${raleway.className} font-black italic text-5xl xl:text-5xl lg:text-6xl md:text-4xl sm:text-3xl text-center`}
+						/>
+
+						<div className='w-4/5 grid grid-cols-2 gap-4 items-center'>
+							{productDescriptionArr.map((p, i) => (
+								<CardWIthOverlay
+									{...p}
+									key={i}
+								/>
+							))}
+						</div>
+					</div>
+				</section>
 			</main>
 		</>
 	);
